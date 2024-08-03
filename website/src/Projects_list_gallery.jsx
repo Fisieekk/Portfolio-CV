852b17b9f657f21658ba7bdcb623216d8d4aa06b
Mserafin <Marcin.serafin0325@gmail.com>
2024-08-03 11:20:34 +0200

    Project List Full

--- website/src/Projects_list_gallery.jsx
+++ website/src/Projects_list_gallery.jsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Card, Typography, Box, IconButton } from "@mui/material";
 import { ChevronRight } from "@mui/icons-material";
-import './Projects_list_gallery.css'; // Import the CSS file with the animation styles
+import './Projects_list_gallery.css';
 
 const projects_images = [
     { url: "src/assets/JustGithub_img.png", title: "Just Github", desc: "Just Github", link: "https://github.com/Fisieekk"},
@@ -14,114 +14,96 @@ const projects_images = [
 
 const Projects_list_gallery = () => {
     const [currentBatch, setCurrentBatch] = useState(0);
-    const [prevBatch, setPrevBatch] = useState(0); // Track previous batch for animation
     const batchSize = 3;
+    const numberOfBatches = Math.ceil(projects_images.length / batchSize);
 
-    const currentProjects = projects_images.slice(
-        currentBatch * batchSize,
-        (currentBatch + 1) * batchSize
-    );
+    const handleWheel = useCallback((event) => {
+        event.preventDefault();
+        const { deltaY } = event;
 
-    const [y, setY] = useState(window.scrollY);
-
-    const handleNavigation = useCallback(
-        e => {
-            const window = e.currentTarget;
-            if (y > window.scrollY) {
-                if (currentBatch > 0) {
-                    setPrevBatch(currentBatch);
-                    setCurrentBatch(prevBatch => prevBatch - 1);
-                }
-            } else if (y < window.scrollY) {
-                if ((currentBatch + 1) * batchSize < projects_images.length) {
-                    setPrevBatch(currentBatch);
-                    setCurrentBatch(prevBatch => prevBatch + 1);
-                }
-            }
-            setY(window.scrollY);
-        }, [y, currentBatch, prevBatch]
-    );
+        if (deltaY > 0 && currentBatch < numberOfBatches - 1) {
+            setCurrentBatch(currentBatch + 1); // Scroll down
+        } else if (deltaY < 0 && currentBatch > 0) {
+            setCurrentBatch(currentBatch - 1); // Scroll up
+        }
+    }, [currentBatch, numberOfBatches]);
 
     useEffect(() => {
-        setY(window.scrollY);
-        window.addEventListener("scroll", handleNavigation);
-
+        window.addEventListener("wheel", handleWheel, { passive: false });
         return () => {
-            window.removeEventListener("scroll", handleNavigation);
+            window.removeEventListener("wheel", handleWheel);
         };
-    }, [handleNavigation]);
+    }, [handleWheel]);
+
+    console.log(currentBatch);
 
     return (
-        <Box display="flex" flexDirection="column" alignItems="center" bgcolor="black" p={2} height="86vh">
-            {currentProjects.map((image, index) => {
-                // Determine if the current image should slide in or out
-                const isVisible = index >= currentBatch * batchSize && index < (currentBatch + 1) * batchSize;
-                const isPreviousBatch = index >= prevBatch * batchSize && index < (prevBatch + 1) * batchSize;
-                let animationClass = '';
-
-                if (isVisible) {
-                    animationClass = 'slide-in';
-                } else if (isPreviousBatch) {
-                    animationClass = 'slide-out';
-                }
-
-                return (
-                    <Card key={index} sx={{ width: '80%', mb: 2, position: 'relative', overflow: 'hidden', borderRadius: '25px' }}>
-                        <a href={image.link} target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>
-                            <Box
-                                sx={{
-                                    height: '25vh',
-                                    backgroundImage: `url(${image.url})`,
-                                    backgroundSize: 'cover',
-                                    backgroundPosition: 'center',
-                                    borderRadius: '20px',
-                                    position: 'relative',
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    justifyContent: 'flex-end',
-                                    color: 'white',
-                                    textShadow: '1px 1px 4px rgba(0, 0, 0, 0.8)',
-                                    padding: '16px',
-                                }}
-                                className={animationClass}
-                            >
+        <Box display="flex" flexDirection="column" alignItems="center" bgcolor="black" p={2} height="86vh" overflow="hidden">
+            {Array.from({ length: numberOfBatches }).map((_, batchIndex) => (
+                <Box key={batchIndex} className={`sector ${currentBatch === batchIndex ? 'slide-in' : 'slide-out'}`}>
+                    {projects_images.slice(batchSize * batchIndex, batchSize * (batchIndex + 1)).map((image, index) => (
+                        <Card key={index} sx={{
+                            width: '80%',
+                            mb: 2,
+                            position: 'relative',
+                            overflow: 'hidden',
+                            borderRadius: '25px',
+                        }}>
+                            <a href={image.link} target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>
                                 <Box
                                     sx={{
-                                        position: 'absolute',
-                                        top: 0,
-                                        left: 0,
-                                        width: '100%',
-                                        height: '100%',
-                                        background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 139, 0.5))',
+                                        height: '25vh',
+                                        backgroundImage: `url(${image.url})`,
+                                        backgroundSize: 'cover',
+                                        backgroundPosition: 'center',
                                         borderRadius: '20px',
-                                        zIndex: 1,
-                                    }}
-                                />
-                                <Box sx={{ position: 'relative', zIndex: 2 }}>
-                                    <Typography variant="h5" component="div">
-                                        {image.title}
-                                    </Typography>
-                                    <Typography variant="body2">
-                                        {image.desc}
-                                    </Typography>
-                                </Box>
-                                <IconButton
-                                    sx={{
-                                        position: 'absolute',
-                                        top: '50%',
-                                        right: '16px',
-                                        transform: 'translateY(-50%)',
+                                        position: 'relative',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        justifyContent: 'flex-end',
                                         color: 'white',
-                                        zIndex: 2,
+                                        textShadow: '1px 1px 4px rgba(0, 0, 0, 0.8)',
+                                        padding: '16px',
                                     }}
                                 >
-                                    <ChevronRight onClick={() => window.open(image.link)} />
-                                </IconButton>
-                            </Box>
-                        </a>
-                    </Card>
-                );
-            })}
+                                    <Box
+                                        sx={{
+                                            position: 'absolute',
+                                            top: 0,
+                                            left: 0,
+                                            width: '100%',
+                                            height: '100%',
+                                            background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 139, 0.5))',
+                                            borderRadius: '20px',
+                                            zIndex: 1,
+                                        }}
+                                    />
+                                    <Box sx={{ position: 'relative', zIndex: 2 }}>
+                                        <Typography variant="h5" component="div">
+                                            {image.title}
+                                        </Typography>
+                                        <Typography variant="body2">
+                                            {image.desc}
+                                        </Typography>
+                                    </Box>
+                                    <IconButton
+                                        sx={{
+                                            position: 'absolute',
+                                            top: '50%',
+                                            right: '16px',
+                                            transform: 'translateY(-50%)',
+                                            color: 'white',
+                                            zIndex: 2,
+                                        }}
+                                    >
+                                        <ChevronRight onClick={() => window.open(image.link)} />
+                                    </IconButton>
+                                </Box>
+                            </a>
+                        </Card>
+                    ))}
+                </Box>
+            ))}
         </Box>
     );
 };
